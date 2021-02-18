8e719661ff4f620fc1fd9e87f55f7c07ed256093
Suwon Park <sepiabrown@naver.com>
2021-02-18 17:12:18 +0900

    sync:

--- .spacemacs.d/private/local/bulletins-mode/bulletins-mode.el
+++
@@ -1,7 +0,0 @@
-;;;###autoload
-(defun bulletins-mode/sample-function ()
-  "If you can see this with M-x, you've set up your local package properly."
-  (interactive)
-  (message "bulletins-mode is present!"))
-
-(provide 'bulletins-mode)
