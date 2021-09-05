f93e8028aa39ba9eb0c724c7c3b1eb09e33ded83
sepiabrown <sepiabrown@naver.com>
2021-09-05 20:55:57 +0900

    startup-system.sh

--- startup-system.sh
+++ startup-system.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # pushd ~/.dotfiles
 pushd /run/media/nixos/USB_DATA/.dotfiles
-sudo nixos-rebuild switch -I nixos-config=./system/configuration.nix_startup
+sudo nixos-rebuild switch -I nixos-config=./system/configuration.nix_current
 popd
