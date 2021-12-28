89531618bbc82e0f65869a358ce80b6b038330d3
Su Won Park <sepiabrown@naver.com>
2021-12-28 20:26:20 +0900

    erase configuration.nix

--- system/apply-flake.sh
+++ system/apply-flake.sh
@@ -5,9 +5,9 @@ pushd ~/dotfiles/system
 if [ $# -eq 0 ]
   then
     # sudo nixos-rebuild switch -I nixos-config="${CONFIG_PATH}" --flake .#
-    sudo nixos-rebuild switch --flake .#
+    sudo nixos-rebuild switch --flake .#sepiabrown-nix
   else
     # sudo nixos-rebuild switch -I nixos-config="${CONFIG_PATH}" -p $1 --flake .#  #--show-trace
-    sudo nixos-rebuild switch -p $1 --flake .#  --show-trace
+    sudo nixos-rebuild switch -p $1 --flake .#sepiabrown-nix --show-trace
 fi
 popd
