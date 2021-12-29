358b422d01d222d2e91542cc92ad7b9cda3047f7
Su Won Park <sepiabrown@naver.com>
2021-12-29 16:03:46 +0900

    211229_fix_home_file_git_status2

--- configuration_current.nix
+++ configuration_current.nix
@@ -170,15 +170,13 @@ done
 #!/bin/sh
 # CONFIG_PATH="./system/configuration_current.nix"
 pushd ~/dotfiles
-if [ -z $1 ]; then
+if [ -z ''$1 ]; then
   echo "needs at least one command"
-  # sudo nixos-rebuild switch -I nixos-config="${CONFIG_PATH}" --flake .#
-  # sudo nixos-rebuild switch --flake 
-elif [ -z $2 ]; then
-  # sudo nixos-rebuild switch -I nixos-config="${CONFIG_PATH}" -p $1 --flake .#  #--show-trace
-  sudo nixos-rebuild switch --flake $1
+  # sudo nixos-rebuild switch -I nixos-config="''${CONFIG_PATH}" --flake .#
+elif [ -z ''$2 ]; then
+  sudo nixos-rebuild switch --flake ''$1
 else
-  sudo nixos-rebuild switch --flake $1 -p $2 --show-trace
+  sudo nixos-rebuild switch --flake ''$1 -p ''$2 --show-trace
 fi
 popd
       '';
