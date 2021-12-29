2ed7647ec11853399981f7ab3f5bf936f11db88b
Su Won Park <sepiabrown@naver.com>
2021-12-29 15:58:30 +0900

    211229_fix_home_file_git_status

--- configuration_current.nix
+++ configuration_current.nix
@@ -168,16 +168,17 @@ done
       '';
       "apply-flake.sh".source = pkgs.writeScript "apply-flake_sh" ''
 #!/bin/sh
-# pushd ~/.dotfiles
 # CONFIG_PATH="./system/configuration_current.nix"
-pushd ~/dotfiles/system
-if [ $# -eq 0 ]
-  then
-    # sudo nixos-rebuild switch -I nixos-config="''${CONFIG_PATH}" --flake .#
-    sudo nixos-rebuild switch --flake .#
-  else
-    # sudo nixos-rebuild switch -I nixos-config="''${CONFIG_PATH}" -p $1 --flake .#  #--show-trace
-    sudo nixos-rebuild switch -p $1 --flake .#  --show-trace
+pushd ~/dotfiles
+if [ -z $1 ]; then
+  echo "needs at least one command"
+  # sudo nixos-rebuild switch -I nixos-config="${CONFIG_PATH}" --flake .#
+  # sudo nixos-rebuild switch --flake 
+elif [ -z $2 ]; then
+  # sudo nixos-rebuild switch -I nixos-config="${CONFIG_PATH}" -p $1 --flake .#  #--show-trace
+  sudo nixos-rebuild switch --flake $1
+else
+  sudo nixos-rebuild switch --flake $1 -p $2 --show-trace
 fi
 popd
       '';
