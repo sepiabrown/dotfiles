2ce8769f151427267bf2776bfcaa1d755dfed9c1
Su Won Park <sepiabrown@naver.com>
2022-01-11 01:02:32 +0900

    minor fix

--- crd/chrome-remote-desktop.nix
+++ crd/chrome-remote-desktop.nix
@@ -11,15 +11,6 @@ in {
       '';
       example = "alice";
     };
-    screenSize = mkOption {
-        type = types.str;
-        description = ''
-            Size of the screen (for all clients)
-        '';
-        default = "1920x1080";
-        example = "1366x768";
-    };
-    newSession = mkEnableOption "new session";
   };
 
   config = mkIf cfg.enable {
@@ -28,7 +19,6 @@ in {
         "chromium/native-messaging-hosts/com.google.chrome.remote_assistance.json".source = "${pkgs.chrome-remote-desktop}/etc/opt/chrome/native-messaging-hosts/com.google.chrome.remote_assistance.json";
         "chromium/native-messaging-hosts/com.google.chrome.remote_desktop.json".source = "${pkgs.chrome-remote-desktop}/etc/opt/chrome/native-messaging-hosts/com.google.chrome.remote_desktop.json";
       };
-
       systemPackages = [ pkgs.chrome-remote-desktop ];
     };
 
@@ -47,49 +37,30 @@ in {
 
     users.users.${cfg.user}.extraGroups = [ "chrome-remote-desktop" ];
 
-    # systemd.user.services.chrome-remote-desktop = { # doesn't work
-    # systemd.services.chrome-remote-desktop@sepiabrown = { # doesn't work
-    systemd.services."chrome-remote-desktop@sepiabrown" = { # works!
+    systemd.packages = [
+      pkgs.chrome-remote-desktop
+    ];
+    
+    # Reference : ${pkgs.chrome-remote-desktop}/lib/systemd/system/chrome-remote-desktop@.service
+    systemd.services."chrome-remote-desktop@sepiabrown" = {
       enable = true;
-      description = "Chrome Remote Desktop instance for a ${cfg.user}";
+      description = "Chrome Remote Desktop instance for ${cfg.user}";
       after = [ "network.target" ];
       serviceConfig = {
         Type = "simple";
         User = cfg.user;
         Environment = "XDG_SESSION_CLASS=user XDG_SESSION_TYPE=x11";
-        # Environment = { 
-        #   XDG_SESSION_CLASS = "user";
-        #   XDG_SESSION_TYPE = "x11";
-        # };
         PAMName = "chrome-remote-desktop";
         TTYPath = "/dev/chrome-remote-desktop";
-        ExecStart = "${pkgs.chrome-remote-desktop}/opt/google/chrome-remote-desktop/chrome-remote-desktop --start --new-session"; #--size=${cfg.screenSize}" + optionalString (cfg.newSession) " --new-session";
+        ExecStart = "${pkgs.chrome-remote-desktop}/opt/google/chrome-remote-desktop/chrome-remote-desktop --start --new-session";
         ExecReload = "${pkgs.chrome-remote-desktop}/opt/google/chrome-remote-desktop/chrome-remote-desktop --reload";
         ExecStop = "${pkgs.chrome-remote-desktop}/opt/google/chrome-remote-desktop/chrome-remote-desktop --stop";
-        # Log output to the journal
         StandardOutput = "journal";
-        # Use same fd as stdout
         StandardError = "inherit";
-        # Must be kept in sync with RELAUNCH_EXIT_CODE in linux_me2me_host.py
         RestartForceExitStatus = "41";
       };
       wantedBy = [ "multi-user.target" ];
     };
-    systemd.packages = [
-      pkgs.chrome-remote-desktop
 
-      # (pkgs.runCommandNoCC "chrome-remote-desktop-link" {
-      #   preferLocalBuild = true;
-      #   allowSubstitutes = false;
-      # } ''
-      #   mkdir -p $out/etc/systemd/system/
-      # '')
-      #  cp ${pkgs.chrome-remote-desktop}/lib/systemd/system/chrome-remote-desktop@.service $out/etc/systemd/system/chrome-remote-desktop@sepiabrown.service
-      #'')  
-      #  cp ${config.systemd.package}/example/systemd/system/chrome-remote-desktop@.service $out/etc/systemd/system/chrome-remote-desktop@sepiabrown.service
-      # ln -s /etc/systemd/system/chrome-remote-desktop@.service $out/etc/systemd/system/chrome-remote-desktop@sepiabrown.service
-    ];
-
-  # systemd.services."chrome-remote-desktop@sepiabrown".wantedBy = [ "multi-user.target" ];
   };
 }
