c31d4a628615ab1654c61ad67f76d7492ba366ca
sepiabrown <sepiabrown@naver.com>
2022-02-01 02:44:13 +0900

    220201_bash_setting

--- homemanager_basic.nix
+++ homemanager_basic.nix
@@ -24,7 +24,11 @@
   #   $ cargo install nix-user-chroot
   #   $ nix-user-chroot ~/.nix bash -c "sh <(curl -L https://github.com/numtide/nix-unstable-installer/releases/download/nix-2.7.0pre20220127_558c4ee/install)"
   #   log in again or type $ . /home/sepiabrown/.nix-profile/etc/profile.d/nix.sh
-  #   $ nix-user-chroot ~/.nix bash -l # need it every time logging in
+  #     - Login procedure
+  #     $ ssh -p 7777 -t sepiabrown@snubayes.duckdns.org "./.cargo/bin/nix-user-chroot ~/.nix bash -l"     
+  #
+  # After installing flake:
+  # $ git update-index --skip-worktree flake.lock
   #
   # search: https://rycee.gitlab.io/home-manager/options.html
   # xsession.enable = true; # needed for graphical session related services such as xscreensaver
@@ -40,6 +44,7 @@
     #  exec ${pkgs.nixUnstable}/bin/nix --experimental-features "nix-command flakes" "$@"
     #'')
     nixUnstable
+    cargo
     wget
     curl
     dig
@@ -132,8 +137,104 @@
     bash = {
       enable = true;
       historyControl = [ "ignoredups" "ignorespace" ];
-      
+      shellAliases = {
+        ls =  "ls --color=auto";
+        grep = "grep --color=auto";
+        fgrep = "fgrep --color=auto";
+        egrep = "egrep --color=auto";
+        ll = "ls -alF";
+        la = "ls -A";
+        l = "ls -CF";
+        alert = "notify-send --urgency=low -i \"$([ $? = 0 ] && echo terminal || echo error)\" \"$(history|tail -n1|sed -e 's/^\\s*[0-9]\\+\\s*//;s/[;&|]\\s*alert$//')\"";
+      }; # single quote needs less backslash!!! unintuitive!!!
+      profileExtra = ''
+# if running bash
+if [ -n ''$BASH_VERSION ]; then
+    # include .bashrc if it exists
+    if [ -f ''$HOME/.bashrc ]; then
+        . ''$HOME/.bashrc
+    fi
+fi
+
+# set PATH so it includes user's private bin if it exists
+if [ -d ''$HOME/bin ] ; then
+    PATH=''$HOME/bin:''$PATH
+fi
+
+# set PATH so it includes user's private bin if it exists
+if [ -d ''$HOME/.local/bin ] ; then
+    PATH=''$HOME/.local/bin:''$PATH
+fi
+
+export PATH
+
+if [ -e ''$HOME/.cargo/env ]; then . ''$HOME/.cargo/env; fi 
+
+if [ -e ''$HOME/.nix-profile/etc/profile.d/nix.sh ]; then . ''$HOME/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
+      '';
+      bashrcExtra = ''
+case $- in
+    *i*) ;;
+      *) return;;
+esac
+
+[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+
+[ -x /usr/bin/lesspipe.sh ] && eval "$(SHELL=/bin/sh lesspipe.sh)"
+
+if [ -z "''${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
+    debian_chroot=''$(cat /etc/debian_chroot)
+fi
+
+case "''$TERM" in
+    xterm-color|*-256color) color_prompt=yes;;
+esac
+
+#force_color_prompt=yes
+
+if [ -n "''$force_color_prompt" ]; then
+    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+        # We have color support; assume it's compliant with Ecma-48
+        # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+        # a case would tend to support setf rather than setaf.)
+        color_prompt=yes
+    else
+        color_prompt=
+    fi
+fi
+
+if [ "''$color_prompt" = yes ]; then
+    PS1=''\'''${debian_chroot:+(''$debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\''$ '
+else
+    PS1=''\'''${debian_chroot:+(''$debian_chroot)}\u@\h:\w\''$ '
+fi
+unset color_prompt force_color_prompt
+
+# If this is an xterm set the title to user@host:dir
+case "''$TERM" in
+xterm*|rxvt*)
+    PS1="\[\e]0;''$\{debian_chroot:+(''$debian_chroot)\}\u@\h: \w\a\]''$PS1"
+    ;;
+*)
+    ;;
+esac
+
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
+
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+
+if [ -e ''$HOME/.cargo/env ]; then . ''$HOME/.cargo/env; fi 
+      '';
     };
+
     dircolors = {
       enable = true;
     };
